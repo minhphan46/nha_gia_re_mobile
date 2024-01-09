998a9f3b9124056f2b395e9adc176aead4ef85f2
nhao2003 <nhathaodev@gmail.com>
2024-01-10 06:08:39 +0700

    Refactor tab layout in user profile screen

--- lib/features/presentation/modules/user_profile/screens/user_profile_screen.dart
+++ lib/features/presentation/modules/user_profile/screens/user_profile_screen.dart
@@ -260,13 +260,14 @@ class _UserProfileScreenState extends State<UserProfileScreen> {
             ),
           ),
           // tab ==============================================
-          FutureBuilder<List<RealEstatePostEntity>>(
-              future: controller.getAllPosts(),
-              builder: (context, snapshot) {
-                return TabProfile(
-                  data: snapshot.data,
-                );
-              }),
+          Expanded(
+              child: FutureBuilder<List<RealEstatePostEntity>>(
+                  future: controller.getAllPosts(),
+                  builder: (context, snapshot) {
+                    return TabProfile(
+                      data: snapshot.data,
+                    );
+                  })),
         ],
       ),
     );
